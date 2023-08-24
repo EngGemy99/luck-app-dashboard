a4c9b72f4a4114b20da0ec6dc9d75e609f09da17
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-24 17:04:50 +0300

    edir in error message

--- src/Pages/OffersWall/Edit_Offers.jsx
+++ src/Pages/OffersWall/Edit_Offers.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Paper, TextField } from "@mui/material"
+import { Button, Paper, TextField } from "@mui/material";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { editSchema, schema } from "./schema";
@@ -11,126 +11,126 @@ import { addOffersWall, editOffor } from "../../store/Slices/userSlice";
 import { useEffect } from "react";
 
 function Edit_Offers() {
-    const [value, setValue] = useState(0);
-    const [isLoading, setIsLoading] = useState(false);
-    const { id } = useParams()
-    const dispatch =useDispatch()
-    const offer = useSelector((state) => {
-        return state.user?.offers.find(value => value._id == id);
-    });
+  const [value, setValue] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const offer = useSelector((state) => {
+    return state.user?.offers.find((value) => value._id == id);
+  });
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(editSchema),
-    });
-    const onSubmit = async (data) => {
-        const formData = new FormData();
-        formData.append("title", data.title);
-        formData.append("description", data.description);
-        formData.append("url", data.url);
-        // formData.append("offerType", "offersWall");
-        if (data?.image?.length > 0) {
-            formData.append("image", data.image[0]);
-        }
-        setIsLoading(true);
-        try {
-            const res = await LukeApp.patch(`offers/wall/${id}`, formData);
-            console.log(res);
-            ToastMessage("success", res.data.message);
-            dispatch(editOffor({_id:id,data:res.data.result}));
-        } catch (error) {
-            console.log(error);
-            ToastMessage("error", error.response.message);
-        } finally {
-            reset();
-            setIsLoading(false);
-        }
-    };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(editSchema),
+  });
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("url", data.url);
+    // formData.append("offerType", "offersWall");
+    if (data?.image?.length > 0) {
+      formData.append("image", data.image[0]);
+    }
+    setIsLoading(true);
+    try {
+      const res = await LukeApp.patch(`offers/wall/${id}`, formData);
+      console.log(res);
+      ToastMessage("success", res.data.message);
+      dispatch(editOffor({ _id: id, data: res.data.result }));
+    } catch (error) {
+      console.log(error);
+      ToastMessage("error", error.response.data.error);
+    } finally {
+      reset();
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        reset({
-            title: offer?.title,
-            url: offer?.url,
-            description: offer?.description,
-        });
-    }, [reset, offer?.title, offer?.url, offer?.description]);
-    return (
-        <Paper
-            sx={{
-                p: 4,
-            }}
+  useEffect(() => {
+    reset({
+      title: offer?.title,
+      url: offer?.url,
+      description: offer?.description,
+    });
+  }, [reset, offer?.title, offer?.url, offer?.description]);
+  return (
+    <Paper
+      sx={{
+        p: 4,
+      }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="title"
+          error={errors.title}
+          helperText={errors.title?.message}
+          {...register("title")}
+          defaultValue={offer?.title}
+          fullWidth
+          focused
+          variant="outlined"
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          label="description"
+          error={errors.description}
+          helperText={errors.description?.message}
+          {...register("description")}
+          defaultValue={offer?.description}
+          variant="outlined"
+          fullWidth
+          focused
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          error={errors.url}
+          helperText={errors.url?.message}
+          {...register("url")}
+          label="url"
+          defaultValue={offer?.url}
+          variant="outlined"
+          fullWidth
+          focused
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          type="file"
+          label="image"
+          variant="outlined"
+          fullWidth
+          focused
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <Button
+          variant="contained"
+          color="success"
+          type="submit"
+          sx={{
+            color: "inherit",
+          }}
         >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField
-                    label="title"
-                    error={errors.title}
-                    helperText={errors.title?.message}
-                    {...register("title")}
-                    defaultValue={offer?.title}
-                    fullWidth
-                    focused
-                    variant="outlined"
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    label="description"
-                    error={errors.description}
-                    helperText={errors.description?.message}
-                    {...register("description")}
-                    defaultValue={offer?.description}
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    error={errors.url}
-                    helperText={errors.url?.message}
-                    {...register("url")}
-                    label="url"
-                    defaultValue={offer?.url}
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    type="file"
-                    label='image'
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <Button
-                    variant="contained"
-                    color="success"
-                    type="submit"
-                    sx={{
-                        color: "inherit",
-                    }}
-                >
-                    Edit Offer Wall
-                </Button>
-            </form>
-        </Paper>
-    )
+          Edit Offer Wall
+        </Button>
+      </form>
+    </Paper>
+  );
 }
 
-export default Edit_Offers
+export default Edit_Offers;
--- src/Pages/TopOffers/Edit_Top_Offers.jsx
+++ src/Pages/TopOffers/Edit_Top_Offers.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Paper, TextField } from "@mui/material"
+import { Button, Paper, TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { editSchema, schema } from "./schema";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,140 +10,145 @@ import { editTopOffer } from "../../store/Slices/userSlice";
 import LukeApp from "../../Api/config";
 
 function Edit_Top_Offers() {
-    const { id } = useParams()
-    const [isLoading, setIsLoading] = useState(false);
-    const topOffer = useSelector((state) => {
-        return state.user?.topOffers.find(value => value._id == id);
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const topOffer = useSelector((state) => {
+    return state.user?.topOffers.find((value) => value._id == id);
+  });
+
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(editSchema),
+  });
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("point", data.point);
+    formData.append("url", data.url);
+    formData.append("description", data.description);
+    // formData.append("offerType", "offersWall");
+    if (data?.image?.length > 0) {
+      formData.append("image", data.image[0]);
+    }
+    setIsLoading(true);
+    try {
+      const res = await LukeApp.patch(`offers/top/${id}`, formData);
+      ToastMessage("success", res.data.message);
+      dispatch(editTopOffer({ _id: id, data: res.data.result }));
+    } catch (error) {
+      console.log(error);
+      ToastMessage("error", error.response.data.error);
+    } finally {
+      reset();
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    reset({
+      description: topOffer?.description,
+      url: topOffer?.url,
+      point: topOffer?.point,
+      title: topOffer?.title,
     });
+  }, [
+    reset,
+    topOffer?.description,
+    topOffer?.point,
+    topOffer?.title,
+    topOffer?.url,
+  ]);
 
-
-    const dispatch =useDispatch()
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(editSchema),
-    });
-    const onSubmit = async (data) => {
-        const formData = new FormData();
-        formData.append("title", data.title);
-        formData.append("point", data.point);
-        formData.append("url", data.url);
-        formData.append("description", data.description);
-        // formData.append("offerType", "offersWall");
-        if (data?.image?.length > 0) {
-            formData.append("image", data.image[0]);
-        }
-        setIsLoading(true);
-        try {
-            const res = await LukeApp.patch(`offers/top/${id}`, formData);
-            ToastMessage("success", res.data.message);
-            dispatch(editTopOffer({_id:id,data:res.data.result}));
-        } catch (error) {
-            console.log(error);
-            ToastMessage("error", error.response.message);
-        } finally {
-            reset();
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        reset({
-            description: topOffer?.description,
-            url: topOffer?.url,
-            point: topOffer?.point,
-            title: topOffer?.title,
-        });
-    }, [reset, topOffer?.description, topOffer?.point, topOffer?.title, topOffer?.url]);
-
-    return (
-        <Paper
-            sx={{
-                p: 4,
-            }}
+  return (
+    <Paper
+      sx={{
+        p: 4,
+      }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="title"
+          error={errors.title}
+          helperText={errors.title?.message}
+          {...register("title")}
+          fullWidth
+          focused
+          defaultValue={topOffer?.title}
+          variant="outlined"
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          label="description"
+          error={errors.description}
+          helperText={errors.description?.message}
+          {...register("description")}
+          variant="outlined"
+          fullWidth
+          focused
+          defaultValue={topOffer?.description}
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          error={errors.url}
+          helperText={errors.url?.message}
+          {...register("url")}
+          label="url"
+          variant="outlined"
+          fullWidth
+          focused
+          defaultValue={topOffer?.url}
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          error={errors.point}
+          helperText={errors.point?.message}
+          {...register("point")}
+          label="point"
+          variant="outlined"
+          fullWidth
+          focused
+          defaultValue={topOffer?.point}
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <TextField
+          type="file"
+          variant="outlined"
+          fullWidth
+          sx={{
+            mb: 3,
+          }}
+        />
+        <br />
+        <Button
+          variant="contained"
+          color="success"
+          type="submit"
+          sx={{
+            color: "inherit",
+          }}
         >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField
-                    label="title"
-                    error={errors.title}
-                    helperText={errors.title?.message}
-                    {...register("title")}
-                    fullWidth
-                    focused
-                    defaultValue={topOffer?.title}
-                    variant="outlined"
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    label="description"
-                    error={errors.description}
-                    helperText={errors.description?.message}
-                    {...register("description")}
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    defaultValue={topOffer?.description}
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    error={errors.url}
-                    helperText={errors.url?.message}
-                    {...register("url")}
-                    label="url"
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    defaultValue={topOffer?.url}
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    error={errors.point}
-                    helperText={errors.point?.message}
-                    {...register("point")}
-                    label="point"
-                    variant="outlined"
-                    fullWidth
-                    focused
-                    defaultValue={topOffer?.point}
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <TextField
-                    type="file"
-                    variant="outlined"
-                    fullWidth
-                    sx={{
-                        mb: 3,
-                    }}
-                />
-                <br />
-                <Button
-                    variant="contained"
-                    color="success"
-                    type="submit"
-                    sx={{
-                        color: "inherit",
-                    }}
-                >
-                    Edit Top Offer
-                </Button>
-            </form>
-        </Paper>
-    )
+          Edit Top Offer
+        </Button>
+      </form>
+    </Paper>
+  );
 }
 
-export default Edit_Top_Offers
+export default Edit_Top_Offers;
